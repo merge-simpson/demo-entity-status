f0b16226b09947f68498b4ddebf1469eb40692d3
merge-simpson <letsdev@kakao.com>
2025-03-03 22:45:50 +0900

    initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,26 @@
+plugins {
+    java
+    id("org.springframework.boot") version "3.4.3"
+    id("io.spring.dependency-management") version "1.1.7"
+}
+
+group = "me.letsdev"
+version = "0.0.1-SNAPSHOT"
+
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(21)
+    }
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
